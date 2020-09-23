28464307833db0969dcec2eccfb0f23b6d9ae36d
0x4A6F <0x4A6F@users.noreply.github.com>
2020-09-23 15:02:04 +0000

    routinator: 0.6.4 -> 0.8.0-rc1
    Fixes [CVE-2020-17366](https://nvd.nist.gov/vuln/detail/CVE-2020-17366).

--- pkgs/servers/routinator/default.nix
+++ pkgs/servers/routinator/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "routinator";
-  version = "0.6.4";
+  version = "0.8.0-rc1";
 
   src = fetchFromGitHub {
     owner = "NLnetLabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1hbqvjv9h1aghpyrl03w5f4j8gjy6n9lx83rmpsh5p7yd9ahwmf9";
+    sha256 = "sha256:0nz18c4iq8333y7y3lqjglmr2sbprzb4714zw1i6dky6kssg2vqb";
   };
 
-  cargoSha256 = "08lizhag7aqz3a59gaapsymn7sf9c6im1aw64n0r9a5advhwdh18";
+  cargoSha256 = "sha256:174agf89jgak70hmdijzp81vjvf3x8a84bzicgqq7hv6pfq2jjff";
 
   meta = with stdenv.lib; {
     description = "An RPKI Validator written in Rust";
