4530756a0a0b4291f8c8beda3ca5574c2bb19b29
0x4A6F <0x4A6F@users.noreply.github.com>
2021-10-02 09:22:43 +0200

    nixosTests: routinator

--- nixos/tests/routinator.nix
+++ nixos/tests/routinator.nix
@@ -1,31 +1,29 @@
+import ./make-test-python.nix (
+  { pkgs, lib, ... }:
 
-import ./make-test-python.nix ({ pkgs, lib, ...} : {
-  name = "routinator";
-  meta = with pkgs.stdenv.lib.maintainers; {
-    maintainers = [ maintainers."0x4A6F" hexa ];
-  };
-
-  nodes =
-    {
-
-      local_routinator = { pkgs, lib, ... }:
-      {
-        services.routinator = {
-          enable = true;
-          extraConfig = ''
-          '';
-        };
-      };
-
+  {
+    name = "routinator";
+    meta = {
+      maintainers = with lib.maintainers; [ _0x4A6F hexa ];
     };
 
-  testScript =
-    ''
+    nodes =
+      {
+
+        local_routinator = { pkgs, lib, ... }:
+          {
+            services.routinator = {
+              enable = true;
+            };
+          };
+
+      };
+
+    testScript = ''
       start_all()
-
       local_routinator.wait_for_unit("network-online.target")
-
-      local_routinator.wait_for_unit("routinator_init.service")
+      #local_routinator.wait_for_unit("routinator.service")
     '';
-})
+  }
+)
 #  curl -s -L "127.0.0.1:9556/validity?asn=12654&prefix=93.175.147.0/24"
