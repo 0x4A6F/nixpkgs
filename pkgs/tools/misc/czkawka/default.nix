ab7394419f738c754fc265bf39f4a761b07a42ed
0x4A6F <0x4A6F@users.noreply.github.com>
2023-10-22 14:24:19 +0200

    czkawka: add desktop entry

--- pkgs/tools/misc/czkawka/default.nix
+++ pkgs/tools/misc/czkawka/default.nix
@@ -15,6 +15,7 @@
 , xvfb-run
 , testers
 , czkawka
+, makeDesktopItem
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -29,6 +30,15 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-iBO99kpITVl7ySlXPkEg2YecS1lonVx9CbKt9WI180s=";
+
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = "${pname}_gui";
+    icon = pname;
+    desktopName = "Czkawka";
+    comment = "A simple, fast and easy to use app to remove unnecessary files from your computer";
+    categories  = [ "System" "FileTools" ];
+  };
 
   nativeBuildInputs = [
     pkg-config
@@ -50,6 +60,12 @@ rustPlatform.buildRustPackage rec {
   nativeCheckInputs = [
     xvfb-run
   ];
+
+  postInstall = ''
+    install -Dm444 ${desktopItem}/share/applications/czkawka.desktop $out/share/applications/com.github.qarmin.czkawka.desktop
+    install -Dm444 data/icons/com.github.qarmin.czkawka.svg -t $out/share/icons/hicolor/scalable/apps
+    install -Dm444 data/com.github.qarmin.czkawka.metainfo.xml -t $out/share/metainfo
+  '';
 
   checkPhase = ''
     runHook preCheck
