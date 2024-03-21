da3d7c234f4a482e0bf4420b876f8c2a352930cc
0x4A6F <0x4A6F@users.noreply.github.com>
2024-03-21 20:53:58 +0100

    saunafs: init at 4.0.1

---
+++ pkgs/by-name/sa/saunafs/package.nix
@@ -0,0 +1,31 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "saunafs";
+  version = "4.0.1";
+
+  src = fetchFromGitHub {
+    owner = "leil-io";
+    repo = "saunafs";
+    rev = "v${version}";
+    hash = "sha256-R9a0AfG/SS95TQVB34fst05cZpmqupWW/XN+1ehbTf8=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  meta = with lib; {
+    description = "";
+    homepage = "https://github.com/leil-io/saunafs";
+    changelog = "https://github.com/leil-io/saunafs/blob/${src.rev}/NEWS";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ _0x4A6F ];
+    mainProgram = "saunafs";
+    platforms = platforms.all;
+  };
+}
