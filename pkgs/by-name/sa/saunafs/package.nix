d33abb2eb78659f6bb8e8832645b2c1e3b2aecfe
Max Hausch <max.hausch@helsinki-systems.de>
2024-03-22 15:14:56 +0100

    saunafs: fix build

--- pkgs/by-name/sa/saunafs/package.nix
+++ pkgs/by-name/sa/saunafs/package.nix
@@ -1,15 +1,24 @@
 { lib
-, stdenv
 , fetchFromGitHub
 , cmake
+, acl
 , fmt_10
 , spdlog
 , yaml-cpp
 , fuse3
+, pkg-config
+, zlib
+, zstd
+, bzip2
+, lzma
 , boost175
+, judy
+, linux-pam
+, db
+, gcc11Stdenv
 }:
 
-stdenv.mkDerivation rec {
+gcc11Stdenv.mkDerivation rec {
   pname = "saunafs";
   version = "4.0.1";
 
@@ -22,19 +31,36 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     cmake
+    acl
     fmt_10
     spdlog
-    yaml-cpp
+    (yaml-cpp.override { stdenv = gcc11Stdenv; })
     fuse3
-    boost175
+    pkg-config
+    (boost175.override { stdenv = gcc11Stdenv; })
+    judy
+    linux-pam
+    db
+    zlib
+    zstd
+    lzma
+    bzip2
+  ];
+
+  cmakeFlags = [
+    "-DENABLE_CLIENT_LIB=ON"
+    "-DENABLE_NFS_GANESHA=OFF"
+    "-DENABLE_WERROR=ON"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DENABLE_CCACHE=OFF"
   ];
 
   meta = with lib; {
     description = "A distributed POSIX file system inspired by Google File System";
-    homepage = "https://github.com/leil-io/saunafs";
+    homepage = "https://saunafs.com";
     changelog = "https://github.com/leil-io/saunafs/blob/${src.rev}/NEWS";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ _0x4A6F ];
+    maintainers = with maintainers; [ _0x4A6F cheriimoya ];
     mainProgram = "saunafs";
     platforms = platforms.all;
   };
